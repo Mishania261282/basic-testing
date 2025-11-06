73f13f30cdfda8deb678e0c3b9895694a9ea843b
Mikhail Kirpichou <mishania261282@.ru>
2025-11-06 21:42:13 +0300

    feat:add solution for table-test

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,132 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  // Addition cases
+  {
+    a: 1,
+    b: 2,
+    action: Action.Add,
+    expected: 3,
+    description: '1 + 2 should be 3',
+  },
+  {
+    a: 2,
+    b: 2,
+    action: Action.Add,
+    expected: 4,
+    description: '2 + 2 should be 4',
+  },
+  {
+    a: 3,
+    b: 2,
+    action: Action.Add,
+    expected: 5,
+    description: '3 + 2 should be 5',
+  },
+  {
+    a: -1,
+    b: 1,
+    action: Action.Add,
+    expected: 0,
+    description: '-1 + 1 should be 0',
+  },
+  {
+    a: 0,
+    b: 0,
+    action: Action.Add,
+    expected: 0,
+    description: '0 + 0 should be 0',
+  },
+  // Subtraction cases
+  {
+    a: 5,
+    b: 2,
+    action: Action.Subtract,
+    expected: 3,
+    description: '5 - 2 should be 3',
+  },
+  {
+    a: 10,
+    b: 10,
+    action: Action.Subtract,
+    expected: 0,
+    description: '10 - 10 should be 0',
+  },
+  {
+    a: 2,
+    b: 5,
+    action: Action.Subtract,
+    expected: -3,
+    description: '2 - 5 should be -3',
+  },
+  {
+    a: -1,
+    b: -1,
+    action: Action.Subtract,
+    expected: 0,
+    description: '-1 - (-1) should be 0',
+  },
+
+  // Multiplication cases
+  {
+    a: 2,
+    b: 3,
+    action: Action.Multiply,
+    expected: 6,
+    description: '2 * 3 should be 6',
+  },
+  {
+    a: 5,
+    b: 0,
+    action: Action.Multiply,
+    expected: 0,
+    description: '5 * 0 should be 0',
+  },
+  {
+    a: -2,
+    b: 3,
+    action: Action.Multiply,
+    expected: -6,
+    description: '-2 * 3 should be -6',
+  },
+  {
+    a: -2,
+    b: -3,
+    action: Action.Multiply,
+    expected: 6,
+    description: '-2 * -3 should be 6',
+  },
+
+  // Division cases
+  {
+    a: 6,
+    b: 3,
+    action: Action.Divide,
+    expected: 2,
+    description: '6 / 3 should be 2',
+  },
+  {
+    a: 10,
+    b: 2,
+    action: Action.Divide,
+    expected: 5,
+    description: '10 / 2 should be 5',
+  },
+  {
+    a: 0,
+    b: 5,
+    action: Action.Divide,
+    expected: 0,
+    description: '0 / 5 should be 0',
+  },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
-  });
-  // Consider to use Jest table tests API to test all cases above
+  test.each(testCases)(
+    `should perform $action correctly: $description`,
+    ({ a, b, action, expected }) => {
+      const result = simpleCalculator({ a, b, action });
+      expect(result).toBe(expected);
+    },
+  );
 });
