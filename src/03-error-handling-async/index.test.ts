839c3751c36f5c5a5e5b13dd9aaf921bfc4072b4
Mikhail Kirpichou <mishania261282@.ru>
2025-11-07 13:24:13 +0300

    feat:add solution for error handling

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -1,30 +1,52 @@
-// Uncomment the code below and write your tests
-// import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCustomError } from './index';
+import {
+  throwError,
+  throwCustomError,
+  resolveValue,
+  MyAwesomeError,
+  rejectCustomError,
+} from './index';
 
 describe('resolveValue', () => {
   test('should resolve provided value', async () => {
-    // Write your test here
+    const valueToResolve = { data: 'some data' };
+    const resolved = await resolveValue(valueToResolve);
+    expect(resolved).toBe(valueToResolve);
   });
 });
 
 describe('throwError', () => {
   test('should throw error with provided message', () => {
-    // Write your test here
+    const errorMessage = 'Specific error message';
+    // Jest предоставляет утилиту .toThrow() для проверки выбрасываемых ошибок.
+    // Эта утилита ожидает функцию, которая *выбрасывает* ошибку.
+    expect(() => throwError(errorMessage)).toThrow(errorMessage);
   });
 
   test('should throw error with default message if message is not provided', () => {
-    // Write your test here
+    // Если сообщение не передано, должно выброситься "Oops!"
+    expect(() => throwError()).toThrow('Oops!');
   });
 });
 
 describe('throwCustomError', () => {
   test('should throw custom error', () => {
-    // Write your test here
+    // Проверяем, что выбрасывается именно экземпляр нашего класса MyAwesomeError
+    expect(() => throwCustomError()).toThrow(MyAwesomeError);
+    // Также можно проверить сообщение, если это необходимо
+    expect(() => throwCustomError()).toThrow(
+      'This is my awesome custom error!',
+    );
   });
 });
 
 describe('rejectCustomError', () => {
   test('should reject custom error', async () => {
-    // Write your test here
+    // Для асинхронных функций, которые должны отклонить промис (reject),
+    // мы используем .rejects.toThrow()
+    await expect(rejectCustomError()).rejects.toThrow(MyAwesomeError);
+    // Или с сообщением
+    await expect(rejectCustomError()).rejects.toThrow(
+      'This is my awesome custom error!',
+    );
   });
 });
